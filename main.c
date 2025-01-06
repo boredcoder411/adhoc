abf9a49c6bf9c691c98e0bfea691e57e7b4f388d
boredcoder411 <taamarcos@gmail.com>
2025-01-06 12:40:32 +0100

    added debug info

--- main.c
+++ main.c
@@ -224,6 +224,23 @@ void Sign(SignArgs* args) {
     };
     ptr = CodeDirectory_put(&cdir, ptr);
 
+    // Show CodeDirectory information
+    printf("CodeDirectory: %d\n", cdir.magic);
+    printf("Length: %d\n", cdir.length);
+    printf("Version: %d\n", cdir.version);
+    printf("Flags: %d\n", cdir.flags);
+    printf("Hash Offset: %d\n", cdir.hashOffset);
+    printf("Identifier Offset: %d\n", cdir.identOffset);
+    printf("Number of Special Slots: %d\n", cdir.nSpecialSlots);
+    printf("Number of Code Slots: %d\n", cdir.nCodeSlots);
+    printf("Code Limit: %d\n", cdir.codeLimit);
+    printf("Hash Size: %d\n", cdir.hashSize);
+    printf("Hash Type: %d\n", cdir.hashType);
+    printf("Page Size: %d\n", cdir.pageSize);
+    printf("Exec Segment Base: %llu\n", cdir.execSegBase);
+    printf("Exec Segment Limit: %llu\n", cdir.execSegLimit);
+    printf("Exec Segment Flags: %llu\n", cdir.execSegFlags);
+
     // Emit identifier
     size_t idLen = strlen(args->identifier) + 1;
     memcpy(ptr, args->identifier, idLen);
